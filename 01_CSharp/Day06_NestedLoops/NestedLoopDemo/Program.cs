6ac0e1fb4a26ee4f71855cf5b4e0594fc15513db
Mrunali Bhamare <your_email@example.com>
2026-08-06 14:19:17 +0530

    Complete Day 6 Nested Loops and Day 7 Arrays practice

---
+++ 01_CSharp/Day06_NestedLoops/NestedLoopDemo/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        /*1) Write a C# program to print the following pattern using nested for loops.
+            * * * * * *
+
+            * * * * *
+
+            * * * * *
+
+            * * * * *
+
+            * * * * *
+         */
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        /*2) Write a C# program to print the following pattern using nested for loops.
+            *
+            * *
+            * * *
+            * * * *
+            * * * * *
+        */
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+        /*3) Write a C# program to print the following pattern using nested for loops.
+            * * * * *
+            * * * *
+            * * *
+            * *
+            *
+         */
+        for (int i = 5; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+
+        /*4) Write a C# program to print the following pattern using nested for loops.
+            1 2 3 4 5
+
+            1 2 3 4 5
+
+            1 2 3 4 5
+
+            1 2 3 4 5
+
+            1 2 3 4 5
+
+
+         */
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+        /*5) Write a C# program to print the following pattern using nested for loops.
+            1
+
+            1 2
+
+            1 2 3
+
+            1 2 3 4
+
+            1 2 3 4 5
+         */
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+        /*6) Write a C# program to print the following pattern using nested for loops.
+            1
+            2 2
+            3 3 3
+            4 4 4 4
+            5 5 5 5 5
+         */
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01_CSharp/Day07_Arrays/ArrayDemo/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        /* 1) Declare and Print Array*/
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        /*2) Access Elements */
+        string[] names = { "Amit", "Rahul", "Priya", "Neha", "Rohit" };
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+        /* 3) Modify Elements */
+        int[] usernumbers = new int[5];
+        for (int i = 0; i < usernumbers.Length; i++)
+        {
+            Console.WriteLine("Enter number:");
+            usernumbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        foreach (var number in usernumbers)
+        {
+            Console.WriteLine(number);
+        }
+        /*4) Sum of Array Elements */
+        int[] sumArray = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        foreach (var number in sumArray)
+        {
+            sum = sum + number;
+        }
+        Console.WriteLine("Sum of Array Elements:" + sum);
+        /*5) Average of Array Elements */
+        int[] marks = { 50, 60, 70, 80, 90 };
+        int total = 0;
+        foreach (var item in marks)
+        {
+            total += item;
+        }
+        Console.WriteLine("Total of Marks:" + total);
+        int average = total / marks.Length;
+        Console.WriteLine(average);
+        /*6) Find Maximum and Minimum Elements */
+    int[] findmax = { 50, 45, 20, 30, 10, 5 };
+        int max=findmax[0];
+        int min = findmax[0];
+        foreach(var item in findmax)
+        {
+            if(item > max)
+            {
+                max = item;
+            }
+            if(item < min)
+            {
+                min = item;
+            }
+        }
+        Console.WriteLine("MAXIMUM ELEMENT:" + max);
+        Console.WriteLine("MINIMUM ELEMENT:" + min);
+
+        /*7) Search for an array Element */
+        int[] searchElement = { 10,50,20,40,30 };
+        Console.WriteLine("Enter the element to search:");
+        int search=Convert.ToInt32 (Console.ReadLine());
+        bool found = false;
+        foreach(var item in searchElement)
+        {
+            if(item == search)
+            {
+                found = true;
+                break;
+            }
+        }
+        if(found)
+        {
+            Console.WriteLine("Element found in the array.");
+        }
+        else
+        {
+            Console.WriteLine("Element not found in the array.");
+        }
+    }
+}
