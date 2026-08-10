c7227618beaa01b11741014317304e0072af9c2b
Mrunali Bhamare <your_email@example.com>
2026-08-10 15:53:37 +0530

    Completed Day 11 - Constructors and Encapsulation

---
+++ 01_CSharp/Day11_Constructors_Encapsulation/ConstructorDemo/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Employee
+{
+	public Employee()
+	{
+		Console.WriteLine("Default Constructor");
+	}
+	public Employee(string name)
+	{
+		Console.WriteLine("Employee:" + name);
+	}
+	public Employee(string name,int age)
+	{
+		Console.WriteLine("Employee:" + name);
+		Console.WriteLine("Age:" + age);
+	}
+}
---
+++ 01_CSharp/Day11_Constructors_Encapsulation/ConstructorDemo/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Student
+{
+    string name;
+    public Student()
+    {
+        Console.WriteLine("Default Constructor");
+    }
+    public Student(string name)
+    {
+        this.name = name;
+    }
+    public void Display()
+    {
+        Console.WriteLine("Student Name : " +name);
+    }
+}
